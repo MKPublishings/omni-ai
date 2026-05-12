e2e80942d0c32c27fa11caa836f57b3c022ae462
Mirnes <bihmirnes@gmail.com>
2026-05-12 13:01:22 -0400

    EVERYBODY NOW

--- src/image-gen/app/ion-image-pipeline.ts
+++ src/image-gen/app/ion-image-pipeline.ts
@@ -1,6 +1,7 @@
 import { buildionWorkflow } from '../backend/gateway/workflow-builder';
 import { ionClient } from '../backend/gateway/ionClient';
 import { MockionClient } from '../backend/gateway/MockionClient';
+import { readImageGenEnvironment } from '../config/env';
 import { getImageGenerationError } from '../shared/error-codes';
 import { getCheckpointConfig } from '../config/models.config';
 import type {
